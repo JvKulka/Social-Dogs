98b3429af56abadb67244627fc3eac13900a51f1
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-16 15:36:40 -0300

    Header
    Realizado a criação do Header e definido seus estilos.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dgos - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
