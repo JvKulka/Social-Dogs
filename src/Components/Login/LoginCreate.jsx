f5784cc8ca021dcc34e37b4c5f5c4be8eacdd63e
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-16 15:55:13 -0300

    Login Routes
    Iniciado a criação da página de login juntamente das suas rotas.

---
+++ src/Components/Login/LoginCreate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginCreate = () => {
+  return <div>LoginCreate</div>;
+};
+
+export default LoginCreate;
---
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPasswordReset = () => {
+  return <div>LoginPasswordReset</div>;
+};
+
+export default LoginPasswordReset;
