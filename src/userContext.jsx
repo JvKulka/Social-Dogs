cac9172c69b2a5fb8bfb4b7397f4559126cafbf5
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-18 15:07:34 -0300

    userContext

---
+++ src/userContext.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { TOKEN_POST, USER_GET } from "./api";
+
+export const UserContext = React.createContext();
+
+export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    const { url, options } = TOKEN_POST({ username, password });
+    const tokenRes = await fetch(url, options);
+    const { token } = await tokenRes.json();
+    window.localStorage.setItem("token", token);
+    getUser(token);
+  }
+
+  return (
+    <UserContext.Provider value={{ userLogin }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
