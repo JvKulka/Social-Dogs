a388f20285589326d6f1a4c0275586ebc0e818df
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-31 16:28:26 -0300

    Perfil Usuário
    Realizado a criação da página de posts do usuário, a qual é acessada ao clicar no @ de quem postou a foto

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { UserStorage } from "./userContext";
 import User from "./Components/User/User";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
