94efbc94e6fcdfaac52c59e76ab18496e5bce53f
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-31 11:28:52 -0300

    Image Skeleton
    Realizado a criação de um skeleto de imagem para pré carregamento antes da exibição da imagem.

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
+import Image from "../Helper/Image";
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
@@ -8,7 +9,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import PhotoComents from "./PhotoComents";
 import { UserContext } from "../../userContext";
 import PhotoDelete from "./PhotoDelete";
+import Image from "../Helper/Image";
 
 const PhotoContent = ({ data }) => {
   const user = React.useContext(UserContext);
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
