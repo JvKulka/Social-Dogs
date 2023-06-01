6644327a77cbb5692c69460bf8e929839f1f1906
João Victor Kulka <jv.kulka13@gmail.com>
2023-06-01 10:50:57 -0300

    Lazy e Suspense
    Utilizado Lazy e Suspense para carregamento da bibliteca de gráfico apenas quando for acessada a página de estatísticas

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../Hooks/useFetch";
 import { STATS_GET } from "../../api";
 import Loading from "../Helper/Loading";
 import Error from "../Helper/Error";
-import UserStatsGraphs from "./UserStatsGraphs";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +21,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
